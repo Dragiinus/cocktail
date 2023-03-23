04713bde9f41ee168f8733f07d3770af7698a8bd
Dragiinus <melvinmaute@gmail.com>
2023-03-23 15:59:09 +0100

    Layout

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import Service from '@/pages/Service';
 import Contact from '@/pages/Contact';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Error from '@/_utils/Error';
+import Layout from '@/pages/Layout';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home/>}/>
+          <Route element={<Layout/>}>
+            <Route index element={<Home/>} />
 
 
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/service" element={<Service/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+            <Route path="/home" element={<Home/>} />
+            <Route path="/service" element={<Service/>} />
+            <Route path="/contact" element={<Contact/>} />
 
-          <Route path="*" element={<Error/>}/>
+            <Route path="*" element={<Error/>} />
+          </Route>
+          
 
         </Routes>
       </BrowserRouter>
