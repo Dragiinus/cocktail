9d5918f5e902492c2c539ab5b3043d4b1bb5fbce
Dragiinus <melvinmaute@gmail.com>
2023-03-24 20:17:27 +0100

    Service Token et Logout

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+
+import { accountService } from '@/_services/account.service';
 
 import './auth.css'
 
+
 const Login = () => {
+    let navigate = useNavigate()
+    // const [login, setLogin] = useState('')
+    // const [password, setPassword] = useState('')
+    const [credentials, setCredentials] = useState({
+        email: 'melvin@maute.com',
+        password: 'password'
+    })
+
+    const onChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    const onSubmit = (e) => {
+        e.preventDefault()
+        accountService.login(credentials)
+            .then(res => {
+                console.log(res)
+                accountService.saveToken(res.data.acces_token)
+                navigate('/admin')
+            })
+            .catch(error => console.log(error))
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="group">
                 <label htmlFor="login">Identifiant</label>
-                <input type="text" name="login"/>
+                <input type="text" name="email" value={credentials.email} onChange={onChange}/>
             </div>
             <div className="group">
                 <label htmlFor="password">Mot de passe</label>
-                <input type="text" name="password"/>
+                <input type="text" name="password" value={credentials.password} onChange={onChange}/>
             </div>
             <div className="group">
                 <button>connexion</button>
